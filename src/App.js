3c74574cd04d21e18b7d7111ddde3cfb7a2e2e7e
rnogara <r.nogara.dev@gmail.com>
2022-04-08 10:28:54 -0300

    terceiro commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+
+import { Container, Head, Titulo } from './styles';
+
+function App() {
+  return (
+    <Container>
+      <Head>
+        <Titulo>Projeto Styled</Titulo>
+      </Head>
+    </Container>
+  );
+}
+
+export default App;
+
+/*<div className="container">
+    <header className="header">
+      <a className='titulo'>Projeto Styled</a>
+    </header>
+
+    <h1>Bem vindo ao sistema!</h1>
+  </div>
+*/
---
+++ src/styles.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    height: 500px;
+`;
+
+export const Head = styled.header`
+    width: 100%;
+    height: 70px;
+    background-color: brown;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
+
+export const Titulo = styled.a`
+    font-size: 25px;
+    color: #fff;
+`;
